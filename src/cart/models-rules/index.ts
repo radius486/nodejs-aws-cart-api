588eff5834c855d6b16afd1633007fb11545c35f
Rodion Grishaev <r.grishaev@softteco.com>
2025-04-06 14:43:53 +0300

    fix: remove not working code.

--- src/cart/models-rules/index.ts
+++ src/cart/models-rules/index.ts
@@ -1,10 +1,4 @@
 import { CartItemEntity } from '../entities/cart-item.entity';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const PRODUCT_ENDPOINT =
-  process.env.PRODUCT_ENDPOINT ||
-  'https://hq21lip4pj.execute-api.eu-west-1.amazonaws.com/prod/products/';
 
 export async function calculateCartTotal(
   items: CartItemEntity[],
@@ -12,37 +6,11 @@ export async function calculateCartTotal(
   try {
     if (!items.length) return 0;
 
-    const prices = await Promise.all(
-      items.map(async ({ product_id }: CartItemEntity) => {
-        const url = `${PRODUCT_ENDPOINT}${product_id}`;
-        return await fetchData(url);
-      }),
-    );
-
-    return items.reduce(
-      (total: number, item: CartItemEntity, index: number) => {
-        return total + prices[index].price * item.count;
-      },
-      0,
-    );
+    return items.reduce((total: number, item: CartItemEntity) => {
+      return total + 100 * item.count;
+    }, 0);
   } catch (error) {
     console.error('Failed to fetch:', error);
-    throw error;
-  }
-}
-
-export async function fetchData(url: string) {
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
     throw error;
   }
 }
